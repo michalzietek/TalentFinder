4bba15eb020d8e5afd9b916be8c8ee2f7049aa8b
michal <michalzietkowski@gmail.com>
2020-11-01 18:54:00 +0100

    [feature/mz] added delete and edit player

--- talentFinder/app_players/urls.py
+++ talentFinder/app_players/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
-from .views import players_list, add_player, ajax_delete_player
+from .views import players_list, add_player, ajax_delete_player, ajax_edit_player
 
 urlpatterns = [
     re_path('^list/$', players_list, name='players_list'),
+    re_path(r'^edit/(?P<pk>\d+)$', ajax_edit_player, name='ajax_edit_player'),
     re_path('^add/$', add_player, name='add_player'),
     re_path('^delete/$', ajax_delete_player, name='delete_player'),
     ]
--- talentFinder/app_players/views.py
+++ talentFinder/app_players/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from datetime import datetime
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.urls import reverse
 
 from app_players.forms import PlayerForm
 from app_players.models import Player
@@ -33,3 +37,33 @@ def ajax_delete_player(request):
     player.delete()
 
     return HttpResponse('')
+
+
+def ajax_edit_player(request, pk):
+    data = {}
+    data['form_is_valid'] = False
+    player = get_object_or_404(Player, pk=pk)
+    form = PlayerForm(instance=player)
+    data_url = reverse('ajax_edit_player', kwargs={'pk': player.pk})
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+        player.name = form.data['name']
+        player.last_name = form.data['last_name']
+        player.club = form.data['club']
+        player.position = form.data['position']
+        player.birth_date = datetime.strptime(form.data['birth_date'], '%d-%m-%Y').strftime("%Y-%m-%d")
+        player.save()
+
+        data['form_is_valid'] = True
+
+        return JsonResponse(data)
+
+
+    data['html_form'] = render_to_string(
+        'forms/edit_player_form.html',
+        {'form': form,
+         'data_url': data_url},
+        request=request
+    )
+
+    return JsonResponse(data)
